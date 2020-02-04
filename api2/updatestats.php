d492a7846ba456d1c922ff71caccc4cc085dfc80
jdkohn <jdkohn18@gmail.com>
2020-02-04 12:41:50 -0500

    updatestats.php

---
+++ api2/updatestats.php
@@ -0,0 +1,27 @@
+<?php
+	$query_string = $_SERVER['QUERY_STRING'];
+	$query_parts = explode('&', $query_string);
+
+	$fields = array();
+
+	foreach($query_parts as $part) {
+		$key_val = explode('=', $part);
+		$fields[$key_val[0]] = $key_val[1];
+	}
+
+	$where_clause = "game_id=" . $fields['game_id'] . " AND player_id=" . $fields["player_id"];
+
+	$update_clause = "";
+
+	foreach(array_keys($fields) as $stat) {
+		if(!in_array($stat, ['game_id', 'player_id'])) {
+			$update_clause = $update_clause . $stat . "=" . $fields[$stat] . " AND ";
+		}
+	}
+
+	$update_clause = substr($update_clause, 0, -5);
+
+	$sql = "UPDATE statline SET " . $update_clause . " WHERE " . $where_clause . ";";
+
+	echo $sql;
+?>
